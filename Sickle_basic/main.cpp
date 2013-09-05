c6da84576ff320fe20abb18adbebd68557f9ca17
rdandekar <ravi.dandekar@gmail.com>
2013-09-05 14:15:59 -0700

    Initial Commit

---
+++ Sickle_basic/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sickle_basic
+//
+//  Created by Ravi Dandekar on 9/5/13.
+//  Copyright (c) 2013 Korf Lab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
